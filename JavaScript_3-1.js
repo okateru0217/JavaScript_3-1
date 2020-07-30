ef5d046f84f0536aa4e73cc0b031939891423337
OkazawaTeruaki <okateru.0217@docomo.ne.jp>
2020-07-30 21:25:29 +0900

    Improve: innerHTML improve textContentImprove: nextId++ improve String Add: Add Todo object

--- JavaScript_3-1.js
+++ JavaScript_3-1.js
@@ -1,3 +1,9 @@
+// ID番号のための変数
+let nextId = 0;
+
+// コメントを格納する配列
+const todos = [];
+
 // DOMの取得
 // コメント入力ボックス
 // 追加ボタン
@@ -6,41 +12,45 @@ const inputComment = document.getElementById('input_todo');
 const addBtn = document.getElementById('add_btn');
 const tableBody = document.getElementById('todo_body');
 
-// ID番号のための変数
-let nextId = 0;
+// ボタン押下時の処理
+addBtn.addEventListener('click', () =>{
+  const todo = {id: nextId++, comment: inputComment.value, status: '作業中', delete: '削除'}
+  todos.push(todo);
+  table(todos);
+});
 
-// コメントを格納する配列
-const todo = [];
+// todosを表示させるための関数
+const table = (todos) => {
+  tableBody.textContent = '';
 
-// タスクを生成する関数
-const table = () => {
+  todos.forEach((todo) => {
+    
     // trの生成
     const tableRecord = document.createElement('tr');
-    tableBody.appendChild(tableRecord);
+    tableBody.appendChild(tableRecord)
     
     // IDの生成
     const tableId = document.createElement('th');
-    tableId.textContent = (nextId++);
+    tableId.textContent = todo.id;
     tableRecord.appendChild(tableId);
-    
+
     // コメントの生成
-    todo.push(inputComment.value);
-    const todoTest = todo[todo.length -1];
-    inputComment.value = '';
-    const tableComment = document.createElement('td');
-    tableComment.textContent = (todoTest);
+    const tableComment = document.createElement('td')
+    tableComment.textContent = todo.comment;
     tableRecord.appendChild(tableComment);
-    
+
     // 「作業中」ボタンの生成
     const tableStatus = document.createElement('td');
-    tableStatus.innerHTML = '<button>作業中</button>';
-    tableRecord.appendChild(tableStatus); 
-    
+    const statusButton = document.createElement('button');
+    statusButton.textContent = todo.status;
+    tableStatus.appendChild(statusButton);
+    tableRecord.appendChild(tableStatus);
+   
     // 「削除」ボタンの生成
     const tableDelete = document.createElement('td');
-    tableDelete.innerHTML = '<button>削除</button>';
-    tableRecord.appendChild(tableDelete); 
-}
-
-// ボタン押下時の処理
-addBtn.addEventListener('click', table);
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = todo.delete;
+    tableDelete.appendChild(deleteButton);
+    tableRecord.appendChild(tableDelete);
+  });
+};
